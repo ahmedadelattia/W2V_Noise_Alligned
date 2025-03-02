82cf9ae31293293b451299693c0c3cb833fb5b37
ahmed <aadel@umd.edu>
2025-03-02 03:31:30 -0500

    Enable mixed precision training by adding fp16 argument to training script

--- train.py
+++ train.py
@@ -85,6 +85,7 @@ def main():
         max_steps=args.num_steps,  # Stop training after 10,000 steps
         per_device_train_batch_size=args.per_device_train_batch_size,
         gradient_accumulation_steps=args.gradient_accumulation_steps,
+        fp16=True,
         logging_dir="./logs",
         logging_steps=100,
         save_steps=500,
